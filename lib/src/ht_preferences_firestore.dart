b74be010e21f9bbd8cdd701e40599a64ad94ce98
fulleni <fulleni@hotmail.com>
2025-04-10 08:10:09 +0100

    feat: initial commit for the library
    - Added basic library structure
    - Created initial test setup
    - Configured CI/CD workflow

---
+++ lib/src/ht_preferences_firestore.dart
@@ -0,0 +1,7 @@
+/// {@template ht_preferences_firestore}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtPreferencesFirestore {
+  /// {@macro ht_preferences_firestore}
+  const HtPreferencesFirestore();
+}
---
+++ test/src/ht_preferences_firestore_test.dart
@@ -0,0 +1,13 @@
+//
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ht_preferences_firestore/ht_preferences_firestore.dart';
+
+void main() {
+  group('HtPreferencesFirestore', () {
+    test('can be instantiated', () {
+      expect(HtPreferencesFirestore(), isNotNull);
+    });
+  });
+}
